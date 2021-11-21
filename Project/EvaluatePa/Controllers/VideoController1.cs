c83b840b80e9ef8f96f6c7afc3eb489afc3a3ebd
kongsakc <kongsakc.apps@gmail.com>
2021-11-21 17:40:57 +0700

    Create VideoController1.cs
    Create Video Controller

---
+++ Project/EvaluatePa/Controllers/VideoController1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvaluatePa.Controllers
+{
+    public class VideoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
