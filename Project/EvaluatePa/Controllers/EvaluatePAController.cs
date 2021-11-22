8f76f63aa8ec1b374ed59ae2b8b7dcdf742cf723
kongsakc <kongsakc.apps@gmail.com>
2021-11-22 08:06:37 +0700

    Evaluate_PA
    EvaluatePA_Index

---
+++ Project/EvaluatePa/Controllers/EvaluatePAController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvaluatePa.Controllers
+{
+    public class EvaluatePAController : Controller
+    {
+        public IActionResult EvaluatePA_Index()
+        {
+            return View();
+        }
+    }
+}
